f56688311b15329c9d6f9e44f5e9edb1440ed548
Avinash Kamat <Avinash_Kamat@epam.com>
2019-05-30 15:17:46 +0530

    partial homepage

---
+++ home-card.js
@@ -0,0 +1,42 @@
+const proxyurl = "https://cors-anywhere.herokuapp.com/";
+function getHeroById(id) {
+    const url = `${proxyurl}https://superheroapi.com/api/2281827108577207/${id}`;
+    console.log(url);
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            var heroes = data;
+            console.log(heroes);
+            var card = document.createElement("div");
+            card.className = "card";
+            var img = document.createElement("img");
+            img.className = "card-img";
+            img.src = heroes.image.url;
+
+            img.setAttribute("alt", heroes.name);
+            card.appendChild(img);
+
+            var cardContainer = document.createElement("div");
+            cardContainer.className = "card-container";
+            var cardHeading = document.createElement("h4");
+            cardHeading.innerText = heroes.name;
+            cardContainer.appendChild(cardHeading);
+
+            var cardDesc = document.createElement("p");
+            cardDesc.innerText = heroes.biography.publisher;
+            cardContainer.appendChild(cardDesc);
+
+            card.appendChild(cardContainer);
+            document.getElementById("heroes-row").append(card);
+
+        })
+        .catch(error => {
+            console.log(error);
+            console.log("Hero Name not found");
+        });
+}
+for (let i = 0; i < 3; i++) {
+    let id = Math.ceil(Math.random() * 731);
+    console.log(id);
+    getHeroById(id);
+}
